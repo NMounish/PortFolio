8631a988c0ded8751aaf8da361338d131798c6c7
NMounish <nachimuthu.mounish@gmail.com>
2025-02-07 10:55:28 +0530

    Updated

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -150,7 +150,7 @@ const Home = () => {
                   
                   <button
                     onClick={() => scrollToSection(section)}
-                    className={`block py-2 px-3 text-gray-900 rounded-lg hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-2 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${
+                    className={`block py-2 px-3 text-gray-900 rounded-lg hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-2 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${
                       activeSection === section ? "bg-blue-700 text-white" : ""
                     }`}
                   >
