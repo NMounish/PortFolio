ddda1404c07e1a345cb2cb60fee70484eeb2a863
NMounish <nachimuthu.mounish@gmail.com>
2025-02-07 10:23:03 +0530

    updated

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -144,17 +144,19 @@ const Home = () => {
             } w-full md:flex md:w-auto md:order-1`}
             id="navbar-sticky"
           >
-            <ul className="flex flex-col p-4 lg:flex lg:ml-96 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:ml-auto md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col p-4 lg:flex lg:ml-72 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:ml-auto md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               {["home", "about", "projects", "contact"].map((section) => (
                 <li key={section}>
+                  
                   <button
                     onClick={() => scrollToSection(section)}
-                    className={`block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${
+                    className={`block py-2 px-3 text-gray-900 rounded-lg hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-2 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${
                       activeSection === section ? "bg-blue-700 text-white" : ""
                     }`}
                   >
                     {section.charAt(0).toUpperCase() + section.slice(1)}
                   </button>
+                  
                 </li>
               ))}
               <li className="flex items-center space-x-3">
