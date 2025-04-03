f4e3f7081042cf7a7bced26efd4d04371fc60626
NMounish <nachimuthu.mounish@gmail.com>
2025-04-03 13:16:22 +0530

    modified skillset

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -15,6 +15,7 @@ import Project1 from "../assets/Projects1.jpg";
 import Project2 from "../assets/Projects2.jpg";
 import Project3 from "../assets/Projects3.jpg";
 import Project4 from "../assets/Projects4.jpg";
+import Project5 from "../assets/Projects5.png";
 import leetcode from "../assets/leetcode.png";
 import hackerrank from "../assets/hackerrank.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -152,7 +153,7 @@ const Home = () => {
 
   return (
     <div>
-      <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+      <nav className="bg-white  fixed w-full z-20 top-0 left-0 border-b border-gray-200 ">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
             PORTFOLIO
@@ -161,14 +162,14 @@ const Home = () => {
             <a
               href="/21CSR115-Mounish-Resume.pdf"
               download="Mounish-Resume.pdf"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center w-full"
             >
               DOWNLOAD
             </a>
             <button
               onClick={toggleMenu}
               type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
               aria-controls="navbar-sticky"
               aria-expanded={isMenuOpen ? "true" : "false"}
             >
@@ -196,12 +197,12 @@ const Home = () => {
             } w-full md:flex md:w-auto md:order-1`}
             id="navbar-sticky"
           >
-            <ul className="flex flex-col p-4 lg:flex lg:ml-72 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:ml-auto md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col p-4 lg:flex lg:ml-72 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:ml-auto md:flex-row md:mt-0 md:border-0 md:bg-white">
               {sections.map((section) => (
                 <li key={section}>
                   <button
                     onClick={() => scrollToSection(section)}
-                    className={`block py-2 px-3 text-gray-900 rounded-lg hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-2 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${
+                    className={`block py-2 px-3 text-gray-900 rounded-lg hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-2${
                       activeSection === section ? "bg-blue-700 text-white" : ""
                     }`}
                   >
@@ -281,7 +282,7 @@ const Home = () => {
         
 
           <div id="about"  ref={setSectionRef("about")} className="text-center mt-96">
-            <h2 className="text-3xl font-bold text-slate-800 dark:text-white mb-8">
+            <h2 className="text-3xl font-bold text-slate-800">
               ABOUT ME
             </h2>
             <p className="text-lg leading-relaxed whitespace-normal">
@@ -307,12 +308,12 @@ const Home = () => {
           </div>
         
           <div id="skills" ref={setSectionRef("skills")} className="py-12 mt-96">
-            <h2 className="text-3xl font-bold text-slate-800 dark:text-white mb-12 text-center">
+            <h2 className="text-3xl font-bold text-slate-800 mb-12 text-center">
               MY SKILLS
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-4xl mx-auto">
               {skills.map((skill, index) => (
-                <div key={index} className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg">
+                <div key={index} className="bg-white  p-6 rounded-lg shadow-lg">
                   <div className="flex justify-between mb-2">
                     <span className="text-lg font-medium text-gray-800 dark:text-white">
                       {skill.name}
@@ -405,6 +406,24 @@ const Home = () => {
                   <p className="text-lg font-medium">Mouse Trial</p>
                   <a
                     href="https://github.com/NMounish/Mouse-Trial-Effect.git"
+                    className="inline-block mt-2 px-4 py-2 bg-blue-500 text-white rounded-full"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    VIEW
+                  </a>
+                </div>
+              </div>
+              <div className="snap-center flex-shrink-0">
+                <img
+                  src={Project5}
+                  className="sm:w-full md:w-96 mx-auto h-auto"
+                  alt="Project 3"
+                />
+                <div className="mt-4 text-center">
+                  <p className="text-lg font-medium">Crowd Funding</p>
+                  <a
+                    href="https://github.com/NMounish/CrowdFunding.git"
                     className="inline-block mt-2 px-4 py-2 bg-blue-500 text-white rounded-full"
                     target="_blank"
                     rel="noopener noreferrer"
